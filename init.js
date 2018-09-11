c692c12c51f58d52d8106ae0d3e8f8a35f0ab77c
lev.akhnazarov <levakhnazarov@gmail.com>
2018-09-11 17:57:59 +0300

    timeout

--- init.js
+++ init.js
@@ -1,15 +1,13 @@
 const Web3 = require('web3')
 const fs = require('fs')
 const csv = require('fast-csv')
-const nodejs_argv = require('nodejs-argv')
+const nodejsArgv = require('nodejs-argv')
 
 
-const argv = nodejs_argv.new()
+const argv = nodejsArgv.new()
 const Big = require('big.js')
 const abi = require('./ABI.json')
 
-
-const NETWORK_GAS = 250000
 
 argv.option([
   ['-c', '--contract', '0x41e5560054824eA6B0732E656E3Ad64E20e94E45', 'contract address'],
@@ -23,8 +21,8 @@ try {
   argv.get('-c')
   argv.get('-p')
   argv.get('-f')
-} catch (e) {
-  throw new Error(e)
+} catch (error) {
+  throw new Error(error)
 }
 
 
@@ -41,60 +39,26 @@ const account = web3.eth.accounts.privateKeyToAccount(privateKey)
 
 const recipientsArr = []
 
-csv
-  .fromPath('file.csv')
-  .on('data', (data) => {
- 	console.log(`${(new Big(`${data[1]}`)).times((new Big(10)).pow(8))}`)
- 	recipientsArr.push({ addr: data[0], amount: `${(new Big(`${data[1]}`)).times((new Big(10)).pow(8))}` })
-  })
-  .on('end', () => {
-    main()
-  })
-
-
-const main = async function () {
-  let nonce = await web3.eth.getTransactionCount(addressFrom)
-  let gasPrice = await web3.eth.getGasPrice()
-
-  gasPrice = +((new Big(gasPrice)).times(gas))
-  // const batch = new web3.BatchRequest()
-  // for (let t in recipientsArr) {
-  //   let coin = recipientsArr[t]
-  //   nonce++
-  //   gasPrice = +((new Big(gasPrice)).times(1.3))
-  //   const contract = new web3.eth.Contract(abi, contractAddress, {from: addressFrom})
-  //   let contractMethod = contract.methods.transfer(coin.addr, coin.amount).encodeABI()
-  //   const transaction = {gasPrice: gasPrice, nonce: nonce, data: contractMethod, to: coin.addr, value: web3.utils.toHex('0x0'), gas: NETWORK_GAS, chainId: ETH_CHAIN_ID}
-  //   const signedTx = await web3.eth.accounts.signTransaction(transaction, privateKey)
-  //   batch.add(web3.eth.sendSignedTransaction.request(signedTx.rawTransaction, 'receipt', console.log))
-  // }
-  // batch.execute()
-  for (const t in recipientsArr) {
-    const coin = recipientsArr[t]
-
-    nonce++
-
-    const contract = new web3.eth.Contract(abi, contractAddress, { from: addressFrom })
-    const contractMethod = contract.methods.transfer(coin.addr, coin.amount).encodeABI()
-
-
-    const send = await sendTransaction(contractAddress, web3.utils.toHex('0x0'), privateKey, 250000, gasPrice, contractMethod, true, nonce, coin.addr)
-
-    console.log(send)
-  }
-}
 
 const sendTransaction = function (address, amount, privateKey, gas, gasPrice, input, isTokenTransaction, nonce, addressFrom) {
   return new Promise((resolve, reject) => {
-    const transaction = { nonce, data: input, gasPrice:Math.floor(gasPrice), to: address, value: amount, gas:Math.floor(gas), chainId: ETH_CHAIN_ID }
-console.log(transaction)
+    const transaction = {
+      nonce,
+      data: input,
+      gasPrice: Math.floor(gasPrice),
+      to: address,
+      value: amount,
+      gas: Math.floor(gas),
+      chainId: ETH_CHAIN_ID,
+    }
+
     web3.eth.accounts.signTransaction(transaction, privateKey)
       .then((signedTransaction) => {
         web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
- 		.once('transactionHash', (hash) => {
- 			console.log([`transferToReceiver Trx Hash:${hash}`])
- 			resolve(1)
- 		})
+          .once('transactionHash', (hash) => {
+            console.log([`transferToReceiver Trx Hash:${hash}`])
+            resolve(1)
+          })
           .once('receipt', (receipt) => { console.log(['transferToReceiver Receipt:', receipt]) })
           .on('confirmation', (confirmationNumber) => { console.log(`transferToReceiver confirmation: ${confirmationNumber}`) })
           .on('error', console.error)
@@ -107,3 +71,47 @@ console.log(transaction)
       })
   })
 }
+
+const main = async function () {
+  let nonce = await web3.eth.getTransactionCount(addressFrom)
+  let gasPrice = await web3.eth.getGasPrice()
+
+  gasPrice = +((new Big(gasPrice)).times(gas))
+  for (let x = 0, ln = recipientsArr.length; x < ln; x++) {
+    setTimeout((coin) => {
+      nonce++
+      console.log(coin)
+      const contract = new web3.eth.Contract(abi, contractAddress, { from: addressFrom })
+      const contractMethod = contract.methods.transfer(coin.addr, coin.amount).encodeABI()
+
+      sendTransaction(contractAddress, web3.utils.toHex('0x0'), privateKey, 250000, gasPrice, contractMethod, true, nonce, coin.addr)
+        .then((send) => {
+          console.log(send)
+        })
+    }, x * 1000, recipientsArr[x])
+  }
+}
+
+csv
+  .fromPath('file.csv')
+  .on('data', (data) => {
+    console.log(`${(new Big(`${data[1]}`)).times((new Big(10)).pow(8))}`, data[0])
+    recipientsArr.push({ addr: data[0], amount: `${(new Big(`${data[1]}`)).times((new Big(10)).pow(8))}` })
+  })
+  .on('end', () => {
+    main()
+  })
+
+
+// const batch = new web3.BatchRequest()
+// for (let t in recipientsArr) {
+//   let coin = recipientsArr[t]
+//   nonce++
+//   gasPrice = +((new Big(gasPrice)).times(1.3))
+//   const contract = new web3.eth.Contract(abi, contractAddress, {from: addressFrom})
+//   let contractMethod = contract.methods.transfer(coin.addr, coin.amount).encodeABI()
+//   const transaction = {gasPrice: gasPrice, nonce: nonce, data: contractMethod, to: coin.addr, value: web3.utils.toHex('0x0'), gas: NETWORK_GAS, chainId: ETH_CHAIN_ID}
+//   const signedTx = await web3.eth.accounts.signTransaction(transaction, privateKey)
+//   batch.add(web3.eth.sendSignedTransaction.request(signedTx.rawTransaction, 'receipt', console.log))
+// }
+// batch.execute()
